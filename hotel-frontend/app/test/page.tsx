0152db7fab656a548fe4d489d851e4f782820986
Pirawish Pathumngern <89306313+peamz4@users.noreply.github.com>
2024-12-03 18:49:55 +0700

    create: component (layout)
    Navbar

---
+++ hotel-frontend/app/test/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar/page";
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="w-screen h-screen">
+        <div className="p-6">
+          <h1 className="">Test Page</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
