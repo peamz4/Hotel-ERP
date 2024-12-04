7b288d25f92104e35ffb73ed5ff37b5df8e66b48
Pirawish Pathumngern <89306313+peamz4@users.noreply.github.com>
2024-12-04 19:31:02 +0700

    create & fix : Navbar with mobile responsive
    That's it

--- hotel-frontend/app/page.tsx
+++ hotel-frontend/app/page.tsx
@@ -1,11 +1,11 @@
 // import Image from "next/image";
 import Navbar from "@/components/navbar/navber";
-
+import Header from "@/components/header";
 
 export default function Home() {
   return (
     <div className="">
-      <Navbar />
+      <Header />
     </div>
   );
 }
--- hotel-frontend/components/navbar/navber.tsx
+++ hotel-frontend/components/navbar/navber.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="w-full h-[100px] bg-[#5C5C5C]">
-        <Image src="/asset/logo/logo.png" className="" alt="Logo" width={50} height={50} />
-        <div className="flex justify-center items-center gap-5">
-          <a href="">Home</a>
-          <a href="">Home</a>
-          <a href="">Home</a>
-          <a href="">Home</a>
+    <nav className="w-screen h-[120px] bg-[#5C5C5C] p-4 pl-12 pr-12  flex flex-row justify-between">
+        <Image src="/logo/logo.png" className="" alt="Logo" width={170} height={130} />
+        <div className="flex justify-center items-center gap-5 text-[18px]">
+          <a href="">HOME</a>
+          <a href="">ACOMMODATION</a>
+          <a href="">OFFERS</a>
+          <a href="">FACILITIES</a>
+          <a href="">GALLERY</a>
         </div>
-      </div>
     </nav>
   );
 };
