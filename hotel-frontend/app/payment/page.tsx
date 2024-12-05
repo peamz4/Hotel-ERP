4c2adb264d2d62a707e1e05df96161a2e49fcdea
nyannnnno <nanometer107@gmail.com>
2024-12-05 23:41:18 +0700

    update user site

--- hotel-frontend/app/payment/page.tsx
+++ hotel-frontend/app/payment/page.tsx
@@ -7,17 +7,9 @@ const Page: React.FC = () => {
     return (
         <div>
             <Navbar />
-            <div className="px-20">
-                <div>
-                    <h1 className="text-[#b4a258] text-3xl sm:text-[40px] leading-normal max-md:flex max-md:justify-center">
-                        PAYMENT
-                    </h1>
-                </div>
-
-                {/* Add the PaymentMethod component here */}
-                <div className="mt-10">
-                    <PaymentMethod />
-                </div>
+            {/* Add the PaymentMethod component here */}
+            <div className="mt-10">
+                <PaymentMethod />
             </div>
             <Footer />
         </div>
