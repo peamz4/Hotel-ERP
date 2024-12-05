9da019790521442f6f3ba7d3072a47f75db3a282
nyannnnno <nanometer107@gmail.com>
2024-12-06 01:14:45 +0700

    update user booking and payment

--- hotel-frontend/components/payment/paymentmethod.tsx
+++ hotel-frontend/components/payment/paymentmethod.tsx
@@ -12,14 +12,14 @@ export default function PaymentMethod() {
         </h1>
       </div>
       <div className="w-full bg-[#5C5C5C] py-5"></div>
-      <div className="md:flex">
-        <div className="p-5 flex flex-col">
+      <div className="md:flex shadow-xl">
+        <div className="p-5 flex flex-col md:w-[1600px]">
           <h2 className="text-lg font-semibold mb-4">Choose a payment method</h2>
           <div className="flex gap-4">
             {/* Credit Card Button */}
             <button
               onClick={() => setActiveTab("CreditCard")}
-              className={`px-4 py-2 border rounded flex flex-col items-center gap-2 ${activeTab === "CreditCard" ? "bg-gray-300" : "bg-white"
+              className={`px-4 py-2 border rounded flex flex-col items-center w-48 gap-2 ${activeTab === "CreditCard" ? "bg-gray-300" : "bg-white"
                 }`}
             >
               <img src="/logo/payment_credit_card.png" alt="Credit Card" className="h-6 w-6" />
@@ -29,7 +29,7 @@ export default function PaymentMethod() {
             {/* Bank Transfer Button */}
             <button
               onClick={() => setActiveTab("BankTransfer")}
-              className={`px-4 py-2 border rounded flex flex-col items-center gap-2 ${activeTab === "BankTransfer" ? "bg-gray-300" : "bg-white"
+              className={`px-4 py-2 border rounded flex flex-col items-center w-48 gap-2 ${activeTab === "BankTransfer" ? "bg-gray-300" : "bg-white"
                 }`}
             >
               <img src="/logo/payment_bank_transfer.png" alt="Bank Transfer" className="h-6 w-6" />
@@ -39,7 +39,7 @@ export default function PaymentMethod() {
             {/* Cash on Delivery Button */}
             <button
               onClick={() => setActiveTab("CashOn")}
-              className={`px-4 py-2 border rounded flex flex-col items-center gap-2 ${activeTab === "CashOn" ? "bg-gray-300" : "bg-white"
+              className={`px-4 py-2 border rounded flex flex-col items-center w-48 gap-2 ${activeTab === "CashOn" ? "bg-gray-300" : "bg-white"
                 }`}
             >
               <img src="/logo/payment_cash_on.png" alt="Cash on Delivery" className="h-6 w-6" />
@@ -59,29 +59,31 @@ export default function PaymentMethod() {
                     className="w-full border rounded p-2"
                   />
                 </div>
-                <div className="mb-3">
-                  <label className="block font-medium mb-1">*Card Number:</label>
-                  <input
-                    type="text"
-                    placeholder="Enter card number"
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="block font-medium mb-1">*Expiration Date:</label>
-                  <input
-                    type="text"
-                    placeholder="MM/YY"
-                    className="w-full border rounded p-2"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="block font-medium mb-1">*CVC:</label>
-                  <input
-                    type="text"
-                    placeholder="CVC"
-                    className="w-full border rounded p-2"
-                  />
+                <div className="md:flex">
+                  <div className="mb-3 md:pr-2">
+                    <label className="block font-medium mb-1">*Card Number:</label>
+                    <input
+                      type="text"
+                      placeholder="Enter card number"
+                      className="w-full border rounded p-2"
+                    />
+                  </div>
+                  <div className="mb-3 md:pr-2">
+                    <label className="block font-medium mb-1">*Expiration Date:</label>
+                    <input
+                      type="text"
+                      placeholder="MM/YY"
+                      className="w-full border rounded p-2"
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="block font-medium mb-1">*CVC:</label>
+                    <input
+                      type="text"
+                      placeholder="CVC"
+                      className="w-full border rounded p-2"
+                    />
+                  </div>
                 </div>
               </div>
             )}
@@ -94,18 +96,17 @@ export default function PaymentMethod() {
             )}
             {activeTab === "CashOn" && (
               <div>
-                <h3 className="text-lg font-medium mb-3">Cash on Delivery</h3>
-                <p className="text-gray-700">You will pay in cash upon delivery.</p>
               </div>
             )}
           </div>
         </div>
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full px-3">
           <h3 className="py-5">Summary</h3>
           <div className="flex justify-between">
             <p>Deluxe Room x1</p>
             <p>THB 5,220.00</p>
           </div>
+          <p>2 nights</p>
           <div className="border border-black"></div>
           <div className="flex justify-between">
             <p>subtotal</p>
@@ -119,7 +120,7 @@ export default function PaymentMethod() {
             <p>total</p>
             <p>THB 12,287.88</p>
           </div>
-          <div className="flex justify-center">
+          <div className="flex justify-center pb-10">
             <button
               className="bg-[#b4a258] text-white w-40 py-4"
             >
